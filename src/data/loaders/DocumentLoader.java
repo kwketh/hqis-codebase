29b7cbae5c04c18e8e689fc7bf910819078fe134
kwketh <keth@kwketh.com>
2014-03-24 13:25:42 +0000

    minor refactoring in DocumentLoader

--- src/data/loaders/DocumentLoader.java
+++ src/data/loaders/DocumentLoader.java
@@ -3,24 +3,30 @@ package data.loaders;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import data.base.Document;
+import fabric.Response;
 
 import java.io.IOException;
 
 public class DocumentLoader
 {
-    private JsonElement m_jsonObject = null;
+    private JsonElement m_jsonElement = null;
 
-    public void loadFromJson(String json)
+    public void loadFromJson(String jsonContents)
     {
         JsonParser parser = new JsonParser();
-        JsonElement rootElement = parser.parse(json);
-        m_jsonObject = rootElement;
+        JsonElement rootElement = parser.parse(jsonContents);
+        m_jsonElement = rootElement;
+    }
+
+    public void loadFromJson(JsonElement jsonElement)
+    {
+        m_jsonElement = jsonElement;
     }
 
     public void loadFromRemote(String id, String type) throws IOException
     {
         fabric.Response response = fabric.Connection.doGetRequest("doc/" + type + "/" + id);
-        m_jsonObject = response.getJsonElement();
+        m_jsonElement = response.getJsonElement();
     }
 
     public void store(String id, String type, String documentJson) throws IOException
@@ -30,6 +36,6 @@ public class DocumentLoader
 
     public <E extends Document> E constructDocument()
     {
-        return data.Factory.constructDocument(m_jsonObject);
+        return data.Factory.constructDocument(m_jsonElement);
     }
 }
