a953d054980d5a61229ae9c9c2869c6d2bcd78a4
kwketh <keth@kwketh.com>
2014-03-25 08:26:52 +0000

    addd connection timeout and friendly error message

--- src/fabric/Connection.java
+++ src/fabric/Connection.java
@@ -2,6 +2,7 @@ package fabric;
 
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -12,6 +13,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Connection
@@ -35,16 +37,34 @@ public class Connection
     static private Response doRequest(HttpUriRequest request) throws IOException
     {
         Response ret = null;
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        CloseableHttpResponse response = httpClient.execute(request);
+        RequestConfig defaultRequestConfig = RequestConfig.custom()
+                .setSocketTimeout(8000)
+                .setConnectTimeout(8000)
+                .setConnectionRequestTimeout(8000)
+                .setStaleConnectionCheckEnabled(true)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(defaultRequestConfig)
+                .build();
+
+        CloseableHttpResponse response = null;
         try {
-            HttpEntity entity = response.getEntity();
-            Integer responseCode = response.getStatusLine().getStatusCode();
-            String responseText = EntityUtils.toString(entity);
-            ret = new Response(responseCode, responseText);
-            EntityUtils.consume(entity);
-        } finally {
-            response.close();
+            response = httpClient.execute(request);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Unable to perform an online request. Please ensure there is internet\nconnectivity or contact system administrator.", "Error", JOptionPane.ERROR_MESSAGE);
+            response = null;
+        }
+        if (response != null) {
+            try {
+                HttpEntity entity = response.getEntity();
+                Integer responseCode = response.getStatusLine().getStatusCode();
+                String responseText = EntityUtils.toString(entity);
+                ret = new Response(responseCode, responseText);
+                EntityUtils.consume(entity);
+            } finally {
+                response.close();
+            }
         }
         return ret;
     }
